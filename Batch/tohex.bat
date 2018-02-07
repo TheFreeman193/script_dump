35ff67b7c855284e7579c1fe7f3bcc6d4c1e8ae9
TheFreeman193 <thefreeman193@aol.co.uk>
2018-02-07 19:54:29 +0000

    Update tohex.bat
    -Write to file from STDIN
    -Updated Regex
    -Hex files now UTF8
    -Better success/fail tracking
    -Cleaned up

--- Batch/tohex.bat
+++ Batch/tohex.bat
@@ -4,70 +4,86 @@ REM Updated January 2018
 if "%~1" EQU "-?" goto help
 if "%~1" EQU "/?" goto help
 setlocal 
-set tmp=%TEMP%\tohex_%random%%random%
-set "inData=%~1"
+set tempf=%TEMP%\tohex_%random%%random%
+set inData=%~1
 set msg=Writing Hex...
+set msg2=Formatting Raw Hex. This may take a while...
+set err=Something went wrong.
+set errf=%err% File not written.
 set filemode=0
-IF "%inData%" EQU "" goto use_stdin
+if "%inData%" EQU "" goto help
 
 if %inData:~0,1%==- goto checknext
 if %inData:~0,1%==/ goto checknext
 goto normal
 :checknext
 if /i %inData:~1,1%==f set filemode=1
+if /i %inData:~1,1%==i set filemode=2
 
 :normal
-if /i %filemode% EQU 1 (
+if %filemode% EQU 1 (
 	if "%~3" EQU "" (
 		REM FILE IN / STDOUT
-		call :makehex "%~2" "%tmp%1" >nul
-		call :sorthex "%tmp%1" "%tmp%2"
-		type "%tmp%2"
-	) ELSE (
+		call :makehex "%~f2" "%tempf%1" >nul
+		call :sorthex "%tempf%1" "%tempf%2" >nul
+		if exist "%tempf%2" ( type "%tempf%2" ) else ( echo %err% 1>&2 )
+	) else (
 		REM FILE IN / FILE OUT
 		if exist "%~f3" goto fileexists
 		echo:%msg%
-		call :makehex "%~2" "%tmp%1"
-		call :sorthex "%tmp%1" "%~3"
-		echo Done.
+		call :makehex "%~f2" "%tempf%1"
+		echo:%msg2%
+		call :sorthex "%tempf%1" "%~f3"
+		if exist "%~f3" ( echo Done. ) else ( echo %errf% 1>&2 )
 	)
-) ELSE (
-	echo:|set /P ="%inData%">"%tmp%1"
+) else if %filemode% EQU 0 (
+	echo:|set /p ="%inData%">"%tempf%1"
 	if "%~2" EQU "" (
 		REM ArgIn / STDOUT
-		call :makehex "%tmp%1" "%tmp%2" >nul
-		call :sorthex "%tmp%2" "%tmp%1"
-		type "%tmp%1"
-	) ELSE (
-		REM ArgIn / FILEOUT 
+		call :makehex "%tempf%1" "%tempf%2" >nul
+		del "%tempf%1"
+		call :sorthex "%tempf%2" "%tempf%1" >nul
+		if exist "%tempf%1" ( type "%tempf%1" ) else ( echo %err% 1>&2 )
+	) else (
+		REM ArgIn / FILE OUT 
 		if exist "%~f2" goto fileexists
 	    echo:%msg%
-		call :makehex "%tmp%1" "%tmp%2"
-		call :sorthex "%tmp%2" "%~2"
-		echo Done.
+		call :makehex "%tempf%1" "%tempf%2"
+		echo:%msg2%
+		call :sorthex "%tempf%2" "%~f2"
+		if exist "%~f2" ( echo Done. ) else ( echo %errf% 1>&2 )
 	)
-)
-goto done
-
-:use_stdin
-REM STDIN / STDOUT
-REM echo Ctrl+Z --^> Enter to terminate:
-setlocal DisableDelayedExpansion
-for /F "tokens=*" %%a in ('findstr /n $') do (
-  set "line=%%a"
-  setlocal EnableDelayedExpansion
-  set "line=!line:*:=!"
-  echo/!line!>>"%tmp%1"
-  endlocal
-)
-call :makehex "%tmp%1" "%tmp%2" >nul
-call :sorthex "%tmp%2" "%tmp%1"
-type "%tmp%1"
+) else if %filemode% EQU 2 (
+	REM echo Ctrl+Z --^> Enter to terminate:
+	setlocal DisableDelayedExpansion
+	for /F "tokens=*" %%j in ('findstr /n $') do (
+	  set line=%%j
+	  setlocal EnableDelayedExpansion
+	  set line=!line:*:=!
+	  echo/!line!>>"%tempf%1"
+	  endlocal
+	)
+	if "%~2" EQU "" (
+		REM STDIN / STDOUT
+		call :makehex "%tempf%1" "%tempf%2" >nul
+		del "%tempf%1"
+		call :sorthex "%tempf%2" "%tempf%1"
+		if exist "%tempf%1" ( type "%tempf%1" ) else ( echo %err% 1>&2 )
+	) else (
+		REM STDIN / FILE OUT
+		if exist "%~f2" goto fileexists
+	    echo:%msg%
+		call :makehex "%tempf%1" "%tempf%2"
+		echo:%msg2%
+		call :sorthex "%tempf%2" "%~f2"
+		if exist "%~f2" ( echo Done. ) else ( echo %errf% 1>&2 )
+	)
+) else echo %err%
 goto done
 
 :done
-if EXIST "%tmp%1" del "%tmp%1"
-if EXIST "%tmp%2" del "%tmp%2"
+if exist "%tempf%1" del "%tempf%1"
+if exist "%tempf%2" del "%tempf%2"
 goto :eof
 
 :fileexists
@@ -80,31 +96,30 @@ goto :eof
 echo TOHEX: Converts text to a raw hexadecimal string.
 echo/
 echo/
-echo TOHEX [text_string] [output_file]
-echo command-name ^| TOHEX
+echo TOHEX text_string [output_file]
 echo TOHEX /F input_file [output_file]
+echo command-name ^| TOHEX /I [output_file]
 echo/
 echo/
 echo text_string   Text string to be converted to hex
 echo input_file    File containing text string to be converted
 echo output_file   File to write output hex to. If not included, output is written to STDOUT
 echo/
-echo Calling TOHEX without parameters will cause it to accept STDIN and write to STDOUT.
 echo TOHEX will not write to files that already exist.
+echo Suitable for files of size 16MB or less.
 echo/
 goto :eof
 
 REM FUNCTIONS =====================================
 :makehex
-SETLOCAL
-	certutil -encodehex "%1" "%2"
-ENDLOCAL
+setlocal
+	certutil -encodehex "%~f1" "%~f2"
+endlocal
 goto :eof
 
 :sorthex
-SETLOCAL
-	powershell -Command "(gc %1) -replace '\w+\t([\s\w]{23})\s\s([\s\w]{23})\s{3}.+','$1 $2' | Out-File -NoClobber -LiteralPath %2" >nul
-	if ERRORLEVEL 1 goto fileexists
-ENDLOCAL
+setlocal
+	powershell -Command "(gc %~f1) -replace '\w+\t([\s\w]{23})\s\s([\s\w]{23})\s{3}.+','$1 $2' | Out-File -NoClobber -Encoding ASCII -LiteralPath %~f2"
+	if errorlevel 1 goto fileexists
+endlocal
 goto :eof
-
